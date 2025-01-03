142333240b66354946b942c64ef05a0b5c411481
samansa <gon.paru5su40.02@gmail.com>
2025-01-03 17:36:59 +0900

    Add a command

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = mypkg.talker:main',
+            'talker = mypkg.omikuji:main',
             'listener = mypkg.listener:main',
         ],
     },
